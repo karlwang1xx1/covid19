80432de9b2b5ce3dcf001b65b47ff691c50ffa3e
karlwang1xx1 <gremwang@yahoo.com>
2022-06-17 19:11:05 -0400

    tuning with predict GUI module

--- covid19/predict.py
+++ covid19/predict.py
@@ -4,8 +4,12 @@ import pandas as pd
 from tkinter import *
 window =Tk()
 window.title("Predict your patient to be (survive) or not to be (die)")
-window.geometry('400x400')
+window.geometry('600x400')
 window.wm_iconbitmap('concertai.ico') # Generals.ico is a filename for the window icon
+
+lbl_error = Label(window, text='',fg="#f00")
+lbl_error.grid(row=0, column=1, columnspan=3, sticky="w", padx=20)
+
 lbl_g = Label(window, text='Gender')
 
 lbl_r = Label(window, text='Race')
@@ -16,8 +20,8 @@ lbl_a = Label(window, text='Age')
 lbl_g.grid(row=1, column=0, sticky="w", padx=20)
 lbl_r.grid(row=1, column=1, sticky="w", padx=20)
 lbl_a.grid(row=1, column=2, sticky="w", padx=20)
-lbl_error = Label(window, text='',fg="#f00")
-lbl_error.grid(row=0, column=1, columnspan=3, sticky="w", padx=20)
+
+
 
 
 #gender = Entry(window) 
@@ -89,6 +93,13 @@ def ok():
     lbl_4["fg"] = color
     return
 
+def reset():    
+    lbl_1["text"] =""
+    lbl_2["text"] =""
+    lbl_3["text"] =""
+    lbl_4["text"] =""
+    age.delete(0, END)
+
 def pred(p):
     color ="#0f0"
     message =""
@@ -103,29 +114,32 @@ v_g = StringVar(window)
 v_g.set(OPTIONS_G[0]) # default value
 
 g = OptionMenu(window, v_g, *OPTIONS_G)
-g.grid(row=1, column=0, sticky="w", padx=20)
+g.grid(row=2, column=0, sticky="w", padx=20)
 
 
 v_r = StringVar(window)
 v_r.set(OPTIONS_R[0]) # default value
 
 r = OptionMenu(window, v_r, *OPTIONS_R)
-r.grid(row=1, column=1, sticky="w", padx=20)
+r.grid(row=2, column=1, sticky="w", padx=20)
 
 age = Entry(window)
-age.grid(row=1, column=2, sticky="w", padx=20)
+age.grid(row=2, column=2, sticky="w", padx=20)
 button = Button(window, text="Predict", command=ok)
-button.grid(row=2,column=2, sticky="w", padx=20, pady=20)
+button.grid(row=3,column=2, sticky="w", padx=20, pady=20)
+
+btn_reset = Button(window, text="Reset", command=reset)
+btn_reset.grid(row=3,column=2, sticky="w", padx=100, pady=20)
 
 lbl_1 = Label(window, text='Model1:')
-lbl_1.grid(row=3, column=1, columnspan=3, sticky="w", padx=20)
+lbl_1.grid(row=4, column=1, columnspan=3, sticky="w", padx=20)
 lbl_2 = Label(window, text='Model2:')
-lbl_2.grid(row=4, column=1, columnspan=3, sticky="w", padx=20)
+lbl_2.grid(row=5, column=1, columnspan=3, sticky="w", padx=20)
 lbl_3 = Label(window, text='Model3:')
-lbl_3.grid(row=5, column=1, columnspan=3, sticky="w", padx=20)
+lbl_3.grid(row=6, column=1, columnspan=3, sticky="w", padx=20)
 lbl_4 = Label(window, text='Model4:')
-lbl_4.grid(row=6, column=1, columnspan=3, sticky="w", padx=20)
+lbl_4.grid(row=7, column=1, columnspan=3, sticky="w", padx=20)
 
-
+    
 window.mainloop()
 
